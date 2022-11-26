b5f1df2aacd157b2c45c50d68eb4a6d5050a4e68
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-26 20:02:28 +0000

    fix linters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Message from './components/Message';
 
 function App() {
   return (
-    <div>
-      <Message />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Message />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMessages } from '../redux/messages/messageSlice';
 
-const Message = () => (
-  <div>Message</div>
-);
+function Message() {
+  const dispatch = useDispatch();
+  const messages = useSelector((state) => state.messages.messages);
+  const status = useSelector((state) => state.messages.status);
+  return (
+    <div>
+      <h1>Random Messages</h1>
+      {status === 'succeeded' && messages.map((message) => <div key={message.id} style={{ color: 'red', padding: '20px' }}>{message.message}</div>)}
+      <button type="button" onClick={() => dispatch(getMessages())}>Get Messages</button>
+    </div>
+  );
+}
 
 export default Message;
