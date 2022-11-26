150b92258c996065b21bc6e5eaa3483f7c77c8b0
Muhyideen Elias <emuhiydeen6@gmail.com>
2022-11-26 19:29:32 +0000

    add messagesSlice

---
+++ src/redux/messageSlice.js
@@ -0,0 +1,26 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3001';
+
+export const getMessages = createAsyncThunk('messages/getMessages', async () => {
+  const response = await axios.get(baseURL);
+  return response.data;
+});
+
+export const messagesSlice = createSlice({
+  name: 'messages',
+  initialState: {
+    messages: [],
+    status: 'idle',
+  },
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(getMessages.fulfilled, (state, action) => {
+      state.messages = action.payload;
+      state.status = 'succeeded';
+    });
+  },
+});
+
+export default messagesSlice.reducer;
